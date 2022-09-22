6c2ccd35bf54ce5b6a2c496b28eaa76f95dbe7b4
darshanthorat99 <113414840+darshanthorat99@users.noreply.github.com>
2022-09-22 19:52:03 +0530

    Add project files.

---
+++ nestedloop/add4.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nestedloop
+{
+    internal class add4
+    {
+        //static void Main(string[] args)
+        //{
+        //    for (int i = 0; i <=5;i++)            {
+        //        for (int j = 5; j >= i; j--)
+        //        {
+        //            Console.Write($"{j}  ");
+        //        }
+        //        Console.WriteLine();
+        //    }
+        //}
+    }
+}
---
+++ nestedloop/add6.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nestedloop
+{
+    internal class add6
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <=5; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write($" ");
+                }
+                for (int k = 1; k <= i; k++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine("");
+            }
+            Console.WriteLine();
+            
+        }
+    }
+}
